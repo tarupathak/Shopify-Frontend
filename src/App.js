9e87af3c0d757b446f26f053c7e0c7243d253a91
tarupathak <113367480+tarupathak@users.noreply.github.com>
2023-07-20 11:07:17 +0530

    Added navbar

--- src/App.js
+++ src/App.js
@@ -6,12 +6,13 @@ function App() {
   return (
     <>
       <BrowserRouter>
+        <Navbar />
         <Routes>
-          <Route path="/" element={<h1>Product Listing Component</h1>}/>
-          <Route path="/add" element={<h1>Product Listing Component</h1>}/>
-          <Route path="/update" element={<h1>Product Listing Component</h1>}/>
-          <Route path="/logout" element={<h1>Product Listing Component</h1>}/>
-          <Route path="/profile" element={<h1>Product Listing Component</h1>}/>
+          <Route path="/" element={<h1>Product Listing Component</h1>} />
+          <Route path="/add" element={<h1>Product Listing Component</h1>} />
+          <Route path="/update" element={<h1>Product Listing Component</h1>} />
+          <Route path="/logout" element={<h1>Product Listing Component</h1>} />
+          <Route path="/profile" element={<h1>Product Listing Component</h1>} />
         </Routes>
       </BrowserRouter>
     </>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,19 +1,32 @@
-import React from 'react'
-import {Link} from 'react-router-dom';
-import './Components.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Components.css";
 
 const Navbar = () => {
   return (
-    <div>
+    <div id="navbar">
+      <div id="left">SHOPIFY</div>
+      <div id="right">
         <ul className="nav">
-            <li><Link to ="/">Products</Link></li>
-            <li><Link to ="/add">Add Products</Link></li>
-            <li><Link to ="/update">Update Products</Link></li>
-            <li><Link to ="/logout">Logout</Link></li>
-            <li><Link to ="/profile">Profile</Link></li>
+          <li>
+            <Link to="/">Products</Link>
+          </li>
+          <li>
+            <Link to="/add">Add Products</Link>
+          </li>
+          <li>
+            <Link to="/update">Update Products</Link>
+          </li>
+          <li>
+            <Link to="/logout">Logout</Link>
+          </li>
+          <li>
+            <Link to="/profile">Profile</Link>
+          </li>
         </ul>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
