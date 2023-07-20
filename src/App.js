30e9c61bb9f395907ba3926f0332ba9cc2f4faa4
tarupathak <113367480+tarupathak@users.noreply.github.com>
2023-07-20 10:30:28 +0530

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from "react";
+import Navbar from "./Components/Navbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
-      App
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1>Product Listing Component</h1>}/>
+          <Route path="/add" element={<h1>Product Listing Component</h1>}/>
+          <Route path="/update" element={<h1>Product Listing Component</h1>}/>
+          <Route path="/logout" element={<h1>Product Listing Component</h1>}/>
+          <Route path="/profile" element={<h1>Product Listing Component</h1>}/>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
