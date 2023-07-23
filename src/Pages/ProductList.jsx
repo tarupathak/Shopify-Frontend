62c5152138adb484ce88e59857e4ad6dc68ab202
tarupathak <113367480+tarupathak@users.noreply.github.com>
2023-07-23 20:01:05 +0530

    Added search option and integrated api of search

--- src/Pages/ProductList.jsx
+++ src/Pages/ProductList.jsx
@@ -25,10 +25,29 @@ const ProductList = () => {
     }
   };
 
+  const searchProduct = async (e) => {
+    console.warn(e.target.value);
+    let key = e.target.value;
+    if (key) {
+      let result = await fetch(`http://localhost:8080/search/${key}`);
+      result = await result.json();
+      if (result) {
+        setProducts(result);
+      }
+    } else {
+      getProducts();
+    }
+  };
+
   console.warn("products", products);
   return (
     <div className="box">
       <h3>Product List</h3>
+      <input
+        type="text"
+        placeholder="Search Product"
+        onChange={searchProduct}
+      />
       <table class="table table-striped">
         <thead>
           <tr>
@@ -36,11 +55,12 @@ const ProductList = () => {
             <th scope="col">Name</th>
             <th scope="col">Price</th>
             <th scope="col">Category</th>
-            <th scope="col">Operation</th>
+            <th scope="col">Delete Product</th>
+            <th scope="col">Update Product</th>
           </tr>
         </thead>
         <tbody>
-          {products &&
+          {products.length > 0 ? (
             products.map((item, index) => {
               return (
                 <tr key={item._id}>
@@ -54,13 +74,16 @@ const ProductList = () => {
                     </button>
                   </td>
                   <td>
-                    <Link to={"/update/"+item._id}>
+                    <Link to={"/update/" + item._id}>
                       <button>Update</button>
                     </Link>
                   </td>
                 </tr>
               );
-            })}
+            })
+          ) : (
+            <h1>No Result Found!</h1>
+          )}
         </tbody>
       </table>
     </div>
