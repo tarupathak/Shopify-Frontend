614f4f656bcba70ef0490b9003bd7c9a6e308c1a
tarupathak <pathaktaru2002@gmail.com>
2024-02-23 21:26:50 +0530

    Added profile page

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -1,14 +1,19 @@
-import React from "react";
 import img from "../Assets/download.jpeg";
 
-const Profile = ({ user }) => {
+const Profile = () => {
+  const auth = localStorage.getItem("user");
   return (
     <div className="profile">
       <h1>Profile Page</h1>
       <div className="profile-info">
-        <p>Name: name</p>
-        <p>Email: email</p>
-        <p>Location: location</p>
+        <p>
+          <span style={{ fontSize: "large", fontWeight: "600" }}>Name:</span>{" "}
+          {JSON.parse(auth).name}
+        </p>
+        <p>
+          <span style={{ fontSize: "large", fontWeight: "600" }}>Email:</span>{" "}
+          {JSON.parse(auth).email}
+        </p>
       </div>
     </div>
   );
